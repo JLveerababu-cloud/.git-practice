0b94a1a27dbc937dc3b620028bdb50258c53e016
veerababu <jlveerababu99@gmail.com>
2024-08-26 13:59:49 +0530

    Delete 03-variables.sh

--- 03-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PERSON1=Raheem
-PERSON2=Suresh
-
-
-echo "$PERSON1::hi $PERSON2 how are you"
-echo "$PERSON2:: i am good what about you"
-echo "$PERSON1:: i am doing good $PERSON2 what's going on"
-echo "$PERSON2::i started lerning shell scripting $PERSON1"
