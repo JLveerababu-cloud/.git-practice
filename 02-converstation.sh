0941fdb68426a2f85ac9c2c4bdf2de4dd9f2509a
veerababu <jlveerababu99@gmail.com>
2024-08-26 13:59:39 +0530

    Delete 02-converstation.sh

--- 02-converstation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "ramesh::hi suresh how are you"
-echo "suresh:: i am good what about you"
-echo "ramesh:: i am doing good suresh what's going on"
-echo :"ramesh::i started lerning shell scripting ramesh"
