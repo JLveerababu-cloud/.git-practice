1ffe24171666c07e8a59b734eb7808516abf09da
veerababu <jlveerababu99@gmail.com>
2024-08-26 16:16:39 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #index starts from 0,size is 3
-FRUITS=("APPLE""KIWI""ORANGE") #array
+FRUITS=("APPLE" "KIWI" "ORANGE") #array
 
  echo "first fruite is: ${FRUITS[0]}"
-  echo "first fruite is: ${FRUITS[1]}"
-   echo "first fruite is: ${FRUITS[2]}"
+  echo "second fruite is: ${FRUITS[1]}"
+   echo "third fruite is: ${FRUITS[2]}"
 
     # shellcheck disable=SC2145
     echo "first fruite is: ${FRUITS[@]}"
