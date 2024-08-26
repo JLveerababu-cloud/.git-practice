e895e0290cfb2b34fe33598695a0b78689e3e484
veerababu <jlveerababu99@gmail.com>
2024-08-26 16:04:38 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+
+# shellcheck disable=SC2162
+read -s USERNAME
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+# shellcheck disable=SC2162
+# shellcheck disable=SC2034
+read -s PASSWORD
+
+echo  "Please entered is: $USERNAME"
