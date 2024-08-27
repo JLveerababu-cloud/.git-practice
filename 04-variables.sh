01d2bfa76b7d4e8875003090434ad74dd04eef7c
veerababu <jlveerababu99@gmail.com>
2024-08-27 07:44:52 +0530

    Delete 04-variables.sh

--- 04-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PERSON1=$1
-PERSON2=$2
-echo "$PERSON1::  Hi ${PERSON2},how are you?"
-echo "${PERSON2}:: Hello $PERSON1.I am fine.how ae you doing?"
-echo "$PERSON1:: i am doing good ${PERSON2}.what;s going on?"
-echo "${PERSON2}:: I satrted lerning shell script $PERSON1"
