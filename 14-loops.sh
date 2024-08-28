0e921993f963724cc954b90429b1d0b2a81ab839
veerababu <jlveerababu99@gmail.com>
2024-08-28 15:33:21 +0530

    functions

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+      each
+       $i
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
+       done
+       
+
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
