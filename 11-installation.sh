159e0c163ecd4d42a43a9e76d2ae5d2761d3a13f
veerababu <jlveerababu99@gmail.com>
2024-08-31 16:05:53 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,7 +9,7 @@ then
   exit 1
   fi
 
-  dnf list installed gittt # Just checking whether installed or not
+  dnf list installed git # Just checking whether installed or not
 
   if [ $? -ne 0 ]
   then 
