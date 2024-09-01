405ff623f6b0eda6e40c4cddfa6644ca5c660813
veerababu <jlveerababu99@gmail.com>
2024-09-01 17:08:58 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -4,16 +4,17 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-USERID=$(id -u)
+mkdir -p $LOGS_FOLDER
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-
+Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "Please run this script with root priveleges"
+        echo "$R Please run this script with root priveleges $N" &>>$LOG_FILE
         exit 1
     fi
 }
@@ -21,11 +22,27 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N"
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
 CHECK_ROOT
+
+for package in $@  #@  refers to all arguments passed to it
+
+do
+  dnf list installed $package &>>$LOG_FILE
+  if [ $? -ne 0 ]
+  then  
+      echo "$package is not installed, going to install it.." &>>$LOG_FILE
+
+   dnf install $package -y &>>$LOG_FILE
+   VALIDATE $? "installing $package"   
+
+   else   
+       echo "$package is alraedy $Y install ..nothing to do $N" &>>$LOG_FILE
+       fi
+done
